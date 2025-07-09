dc8d9e821056fce5b5118bede52b78636e632202
MorpKnight <christoffelsihombing@gmail.com>
2025-07-09 23:08:08 +0700

    Add Japanese localization and refactor i18next configuration
    - Introduced a new YAML localization file for Japanese translations in public/locales/ja/projects.yaml.
    - Removed the old JSON translation file public/locales/ja/translation.json.
    - Created a new YAML translation file public/locales/ja/translation.yaml to replace the deleted JSON file.
    - Updated the i18next configuration to use the new YAML files and adjusted the load path for translations.
    - Enhanced the i18next setup to parse YAML files for localization.

--- src/i18next.js
+++ src/i18next.js
@@ -1,29 +1,30 @@
-// src/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import yaml from 'js-yaml';
 
 i18n
-  .use(Backend)
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     debug: true,
     supportedLngs: ['en', 'ja'],
-    ns: ['translation', 'projects'], // Add 'projects' to the namespaces array
-    defaultNS: 'translation', // Set the default namespace
+    ns: ['translation', 'projects'],
+    defaultNS: 'translation',
     interpolation: {
       escapeValue: false,
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json', // Update loadPath to include the namespace
     },
     detection: {
       order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
       caches: ['localStorage', 'cookie'],
-    }
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.yaml',
+      parse: (data) => yaml.load(data),
+    },
   });
 
 export default i18n;
